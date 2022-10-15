13ca1c3c1ea4346f2b4b1449e892c6239e00b039
redking00 <redking00@hotmail.com>
2022-10-15 12:30:39 +0200

    more tests

--- js/main.js
+++ js/main.js
@@ -1,8 +1,46 @@
 window.onload = () => {
   'use strict';
 
+
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
+    navigator.serviceWorker.register('./sw.js');
   }
+
+  window.addEventListener("online",  () =>{
+    document.querySelector('#offlinescreen').style.display='none';
+  });
+  
+  window.addEventListener("offline", function(){
+    document.querySelector('#offlinescreen').style.display='block';
+  });
+
+  if (!navigator.onLine) {
+    document.querySelector('#offlinescreen').style.display='block';
+  }
+
+
+
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    document.querySelector('#installscreen').style.display='block';
+    document.querySelector('#installscreen>#mainmessage').addEventListener('click',async ()=>{
+      deferredPrompt.prompt();
+      const { outcome } = await deferredPrompt.userChoice;
+      console.log(`User response to the install prompt: ${outcome}`);
+      deferredPrompt = null;      
+      if (`${outcome}`==="accepted") {
+        document.querySelector('#installscreen').style.display='none';
+      }
+    })
+  });
+
+  window.addEventListener('appinstalled', () => {
+    document.querySelector('#installscreen').style.display='none';
+    deferredPrompt = null;
+    console.log('PWA was installed');
+  });
+
 }
