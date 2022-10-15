38dfd9dc92146d4f3b18885a2ea39dcbf407eefb
redking00 <redking00@hotmail.com>
2022-10-15 09:18:31 +0200

    More testing

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+/*
 var cacheName = 'cheapfuel';
 var filesToCache = [
   '/',
@@ -5,22 +6,27 @@ var filesToCache = [
   '/css/style.css',
   '/js/main.js'
 ];
+*/
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
+/*  
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
   self.skipWaiting();
+*/  
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
+/*  
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
   );
+*/  
 });
